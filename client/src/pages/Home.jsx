875445a3872bcdcbdffa19e3c9dd7857aa615f66
Christopher Ahn <chrisahn90@protonmail.com>
2026-02-03 22:16:26 +0000

    Button function testing out displaying number of all articles

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,28 +1,29 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 function Home() {
-  const [newData, setNewsData] = useState([]);
+  const [newsData, setNewsData] = useState(0);
 
   const getNewsData = async () => {
     const res = await axios.get(
-      'https://newsapi.org/v2/everything?q=bitcoin&apiKey=af7a60b8e1274d7a903e6ccc7096c441'
+      'https://newsapi.org/v2/everything?q=trump&apiKey=af7a60b8e1274d7a903e6ccc7096c441'
     );
-    setNewsData(res.data.articles);
+    setNewsData(res.data.totalResults);
   };
 
-  useEffect(() => {
-    getNewsData();
-  }, []);
+  // useEffect(() => {
+  //   getNewsData();
+  // }, []);
 
   const showResults = async () => {
     getNewsData();
   };
 
-  console.log('size: ', newData.length);
+  console.log('size: ', newsData);
 
   return (
     <div className="App">
+      {newsData}
       <button onClick={showResults}>Click Here</button>
     </div>
   );
