ced46ed3499f5955e8217a1e1b58ebc9f9038362
Christopher Ahn <chrisahn90@protonmail.com>
2026-02-08 03:41:57 +0000

    Finally able to display Bar and Line charts

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,33 @@
 // import axios from 'axios';
 // import { useState } from 'react';
-// import { Chart as ChartJS } from 'chart.js/auto';
-// import { Bar, Line, Doughnut } from 'react-chartjs-2';
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js/auto';
 
-import { Bar } from 'react-chartjs-2';
-import sample1 from './datasample/sample1.json';
+import { Bar, Line } from 'react-chartjs-2';
+// import sample1 from './datasample/sample1.json';
 
 // import dates1 from './datasample/dates1.json';
 // import sample2 from './datasample/sample2.json';
 // import sample3 from './datasample/sample1.json';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 function Home() {
   // const [dateLabels2, setDateLabels2] = useState(['2026-01-01']);
@@ -48,17 +67,40 @@ function Home() {
   return (
     <div className="App">
       {/* <div>Line</div> */}
-      <div className="dataCard customerCard">
-        <Bar
+      <div>
+        <Line
           data={{
-            labels: '2026-02-05',
-            datasets: sample1.map((sample) => sample.data),
+            labels: ['2026-01-01', '2026-01-02', '2026-01-03'],
+            datasets: [
+              { label: ['CNN'], data: [900, 1240, 743] },
+              { label: ['AP'], data: [2346, 1512, 1923] },
+            ],
+            backgroundColor: [
+              'rgba(43, 63, 229, 0.8)',
+              'rgba(250, 192, 19, 0.8)',
+              'rgba(253, 135, 135, 0.8)',
+            ],
+            borderRadius: 5,
           }}
         />
       </div>
-      {/* <div>Doughnut</div> */}
-      {/* <button onClick={showResults}>Click Here</button>
-      <button onClick={clearResults}>Clear</button> */}
+      <div>
+        <Bar
+          data={{
+            labels: ['2026-01-01', '2026-01-02', '2026-01-03'],
+            datasets: [
+              { label: ['CNN'], data: [900, 1240, 743] },
+              { label: ['AP'], data: [2346, 1512, 1923] },
+            ],
+            backgroundColor: [
+              'rgba(43, 63, 229, 0.8)',
+              'rgba(250, 192, 19, 0.8)',
+              'rgba(253, 135, 135, 0.8)',
+            ],
+            borderRadius: 5,
+          }}
+        />
+      </div>
     </div>
   );
 }
