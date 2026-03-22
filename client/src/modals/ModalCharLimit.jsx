f0ba8d94ad0faf1d9eaf592d39e1a8d4776d5dbd
Christopher Ahn <chrisahn90@protonmail.com>
2026-03-22 22:32:56 +0000

    Changed modal displays

--- client/src/modals/ModalCharLimit.jsx
+++ client/src/modals/ModalCharLimit.jsx
@@ -3,13 +3,17 @@ import { X } from 'react-feather';
 
 function CharLimit({ isOpen, handleClose }) {
   return (
-    <div className={isOpen ? 'modal display-block' : 'modal display-none'}>
-      <div className="modal-main">
+    <div
+      className={isOpen ? 'modal display-block' : 'modal display-none'}
+      onClick={(e) => {
+        if (e.target.className === 'modal display-block') {
+          handleClose();
+        }
+      }}>
+      <section className="modal-main">
         <X className="closeIcon" onClick={handleClose} />
-        <div>
-          <h3>Character must be between 0 and 15 and must be letters only</h3>
-        </div>
-      </div>
+        <h4>Character must be between 0 and 15 and must be letters only</h4>
+      </section>
     </div>
   );
 }
--- client/src/modals/ModalMinCheckbox.jsx
+++ client/src/modals/ModalMinCheckbox.jsx
@@ -3,13 +3,17 @@ import { X } from 'react-feather';
 
 function MinCheckbox({ isOpen, handleClose }) {
   return (
-    <div className={isOpen ? 'modal display-block' : 'modal display-none'}>
-      <div className="modal-main">
+    <div
+      className={isOpen ? 'modal display-block' : 'modal display-none'}
+      onClick={(e) => {
+        if (e.target.className === 'modal display-block') {
+          handleClose();
+        }
+      }}>
+      <section className="modal-main error">
         <X className="closeIcon" onClick={handleClose} />
-        <div>
-          <h2>Please have at least 1 news check box filled</h2>
-        </div>
-      </div>
+        <h4>Please have at least 1 news check box filled</h4>
+      </section>
     </div>
   );
 }
